cc73da036de1a352651babe4178e8c200d7f62ca
jojobyte <184880+jojobyte@users.noreply.github.com>
2022-09-29 00:39:22 -0600

    Cleanup vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     https: true,
-    cors: {
-      origin: '*',
-    },
     proxy: {
       "/insight": {
         target: "https://insight.dash.org",
@@ -31,36 +27,20 @@ export default defineConfig({
       },
     },
   },
-  preview: {
-    https: true,
-    cors: {
-      origin: '*',
-    },
-  },
   plugins: [
     basicSsl(),
     vue(),
   ],
   optimizeDeps: {
     force: true,
-    exclude: [
-      // 'crowdnode',
-      // '@dashevo/dashcore-lib',
-    ],
-    esbuildOptions: {
-        // Node.js global to browser globalThis
-        define: {
-            global: 'globalThis'
-        },
-        // Enable esbuild polyfill plugins
-        plugins: [
-            NodeGlobalsPolyfillPlugin({
-                buffer: true
-            }),
-        ]
+  },
+  define: {
+    "global": {},
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: false,
+      esmExternals: true,
     },
   },
-  // define: {
-  //   "global": {},
-  // },
 })
